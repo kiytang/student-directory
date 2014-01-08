9ea0c2e7615836b281c59041d57ae58524567821
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-08 15:52:12 +0000

    added user input

--- directory.rb
+++ directory.rb
@@ -1,36 +1,49 @@
-	# let's put all students into an array
-students = [
-	["Dr. Hannibal Lecter", :dicembre],
-	["Darth Vader", :dicembre],
-	["Nurse Ratched", :dicembre],
-	["Michael Corleone", :dicembre],
-	["Alex De Large", :dicembre],
-	["The Alien", :dicembre],
-	["Terminator", :dicembre],
-	["Freddy Kruger", :dicembre],
-	["The Joker", :dicembre],
-	["Mr T", :dicembre]
-]
-
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-------------"
+	puts "The students of my cohort at MA"
+	puts "----------"
 end
 
 def print(students)
 	students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
-	end
+	end	
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+	puts "overall, we have #{names.length} great students"
 end
 
-# nothing happens until we call the methods
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :november}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return the array of students
+	students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+
+
+
+
+
+
+
 
 
 
